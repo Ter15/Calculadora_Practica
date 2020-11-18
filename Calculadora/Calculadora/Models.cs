975399e4a6eb2a33f40c02fd69567d83a1770e54
Ter15 <bonmati20964@gmail.com>
2020-11-18 21:38:53 +0100

    Práctica casi realizada

--- Calculadora/Calculadora/Models.cs
+++ Calculadora/Calculadora/Models.cs
@@ -8,21 +8,79 @@ namespace Calculadora
     {
         public static double GetPower2(int number)
         {
-            if ( number >= 0)
+            if (number >= 0)
             {
                 int result = 1;
                 for (int i = 0; i < number; i++)
-
+                {
                     result *= 2;
+                }
                 return result;
             }
-            else // potencias elevadas a menos algo 
+            else
             {
                 double result = 1.0;
                 for (int i = 0; i < -number; i++)
+                {
                     result /= 2;
+                }
                 return result;
             }
         }
+        public static int GetSummation(int numero)
+        {
+            int i;
+            int result = 0;
+            for (i = 1; i <= numero; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+        public static int GetFactorial(int numero)
+        {
+            int i;
+            int result = 1;
+            for (i = 1; i <= numero; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        public static bool IsPrime(int number)
+        {
+            int i = 2;
+            while (i < number)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+        public static int IsFibonacci(int number)
+        {
+            int a = 1;
+            int aa = 0;
+            int i = 0;
+            System.Console.WriteLine(aa);
+            System.Console.WriteLine(a);
+
+            while (i < number)
+            {
+                System.Console.WriteLine(a + aa);
+
+                int aux = aa;
+                aa = a;
+                a = aux + aa;
+                i++;
+                return a;
+            }
+            return 0;
+        }
+
+
     }
 }
--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -6,19 +6,65 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            UserInterface.PrintMenu();
-            int option = UserInterface.ReadUserOption();
-
-            switch (option)
+            while (true)
             {
-                case 1:
+                UserInterface.PrintMenu();
+                int option = UserInterface.ReadUserOption();
 
-                    UserInterface.PrintMenuPower2();
-                    int numero = UserInterface.ReadUserOption();
-                    Models.GetPower2(numero);
-                    break;
+                switch (option)
+                {
+                    case 1:
+                        UserInterface.PrintMenuPower2();
+                        int number1 = UserInterface.ReadUserOption();
+                        Models.GetPower2(number1);
+                        System.Console.Write("El resultado es: " + Models.GetPower2(number1));
+                        System.Console.Write("\n \n");
+                        // me tengo que esperar a que de a intro
+                        break;
+                    case 2:
+                        UserInterface.PrintMenutSummation();
+                        int number2 = UserInterface.ReadUserOption();
+                        Models.GetSummation(number2);
+                        System.Console.Write("El resultado es: " + Models.GetSummation(number2));
+                        System.Console.Write("\n \n");
+                        // me tengo que esperar a que de a intro
+                        break;
+                    case 3:
+                        UserInterface.PrintMenuFactorial();
+                        int number3 = UserInterface.ReadUserOption();
+                        if (number3 >= 0)
+                        {
+                            Models.GetFactorial(number3);
+                            System.Console.Write("El resultado es: " + Models.GetFactorial(number3));
+                            System.Console.Write("\n \n");
+                            // me tengo que esperar a que de a intro
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("Has introducido un número negativo. Recuerda que el factorial de un número negativo no existe. \n \n");
+                            // me tengo que esperar a que de a intro
+                        }
+                        break;
+                    case 4:
+                        UserInterface.PrintMenuPrime();
+                        int number4 = UserInterface.ReadUserOption();
+                        Models.IsPrime(number4);
+                        System.Console.Write("¿El número introducido es primo? " + Models.IsPrime(number4));
+                        System.Console.Write("\n \n");
+                        // me tengo que esperar a que de a intro
+                        break;
+                    case 5:  //NO SÉ COMO REALIZAR ESTE EJERCICIO
+                        UserInterface.PrintMenuFibonacci();
+                        int number5 = UserInterface.ReadUserOption();
+                        Models.IsFibonacci(number5);
+                        System.Console.Write("¿El número introducido es primo? " + Models.IsFibonacci(number5));
+                        System.Console.Write("\n \n");
+                        // me tengo que esperar a que de a intro
+                        break;
+
+                }
+
             }
-           
 
 
 
